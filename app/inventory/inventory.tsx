aa8e626bcab9dbc3af50620065d3a944510363cb
Mannuel Ferreira <mannuel.ferreira@storebrand.no>
2024-06-16 11:17:22 +0200

    feat: styles added

--- app/inventory/inventory.tsx
+++ app/inventory/inventory.tsx
@@ -11,89 +11,59 @@ import type { InventoryFetchResponse, Listing } from "./inventory.types";
 
 export const Inventory = (data: InventoryFetchResponse): React.ReactElement => {
   return (
-    <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 justify-items-start">
+    <section className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4 justify-items-start">
       {data.listings.map((listing: Listing) => (
-        <article key={listing.release.title} className="flex justify-start">
-          <Card className={cn("p-2 shadow-none w-full overflow-hidden")}>
-            <div className="flex justify-items-start">
+        <article key={listing.release.title} className="flex justify-start w-full">
+          <Card className={cn("p-0 shadow-none w-full overflow-hidden")}>
+            <div className="justify-items-start">
               <CardHeader className="flex-1 p-0">
                 <img
                   className="w-full h-auto min-w-[193px]"
-                  src={listing.release.thumbnail}
+                  src={listing.release.images[0].uri}
                   alt={listing.release.title}
                 />
               </CardHeader>
-              <CardContent className="flex-1">
+              <CardContent className="flex-1 pt-4">
                 <CardTitle className="text-xl">{listing.release.title}</CardTitle>
-                <CardDescription>
+                <CardDescription className="leading-6">
                   <strong>Artist:</strong> {listing.release.artist}
                   <br />
                   <strong>Label:</strong> {listing.release.label} - {listing.release.catalog_number}
                   <br />
                   <strong>Released:</strong> {listing.release.year}
-                  <hr />
+                  <br />
                   <strong>Price:</strong> {listing.original_price.formatted}
-                  <hr />
+                </CardDescription>
+                <CardDescription className="leading-6">
                   <a
                     href={listing.uri}
                     target="_blank"
                     rel="noopener noreferrer"
                     title="🤔 or come into the store save on shipping"
-                    className="text-lg text-blue-500"
+                    className="text-sm text-black-600 border-2 border-black-600 hover:border-black hover:text-color-black rounded-md p-2 mt-2 block"
                   >
                     Buy on Discogs
                   </a>
+                  <span className="flex mt-4">
+                    <span className="flex">
+                      <a
+                        href={`https://music.youtube.com/search?q=${listing.release.title} ${listing.release.artist}`}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="inline-block text-gray-700 grid grid-cols-2 leading-4"
+                      >
+                        <span className="mr-2">Demo</span>
+                        <span>
+                          {" "}
+                          <img src={"./icon-youtube.svg"} alt="YouTube" width={16} height={16} />
+                        </span>
+                      </a>
+                    </span>
+                  </span>
                 </CardDescription>
               </CardContent>
             </div>
-            <CardFooter className="p-0">
-              <br className="p-4" />
-              <strong>🎧 demo:</strong>
-              <div className="flex">
-                <span className="grid grid-cols-6 gap-4	p-2 text-xs font-light text-white rounded-br-lg">
-                  <a
-                    href={`https://music.youtube.com/search?q=${listing.release.artist} ${listing.release.title}`}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="inline-block mr-2"
-                  >
-                    <img src={"./icon-youtube.svg"} alt="YouTube" width={20} height={20} />
-                  </a>
-                  <a
-                    href={`https://music.apple.com/us/search?term=${listing.release.artist} ${listing.release.title}`}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="inline-block mr-2"
-                  >
-                    <img src={"./icon-apple.svg"} alt="Apple Music" width={20} height={20} />
-                  </a>
-                  <a
-                    href={`https://www.deezer.com/search/${listing.release.artist} ${listing.release.title}`}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="inline-block mr-2"
-                  >
-                    <img src={"./icon-deezer.svg"} alt="Deezer" width={20} height={20} />
-                  </a>
-                  <a
-                    href={`https://open.spotify.com/search/${listing.release.artist} ${listing.release.title}`}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="inline-block mr-2"
-                  >
-                    <img src={"./icon-spotify.svg"} alt="Spotify" width={20} height={20} />
-                  </a>
-                  <a
-                    href={`https://listen.tidal.com/search?q=${listing.release.artist} ${listing.release.title}`}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="inline-block mr-2"
-                  >
-                    <img src={"./icon-tidal.svg"} alt="TIDAL" width={20} height={20} />
-                  </a>
-                </span>
-              </div>
-            </CardFooter>
+            <CardFooter className="p-0"></CardFooter>
           </Card>
         </article>
       ))}
