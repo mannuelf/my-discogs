40f260a240334a0acf61538f46d2d5c9f1ed1733
Mannuel Ferreira <mannuel.ferreira@storebrand.no>
2024-06-16 23:09:33 +0200

    fix: tracking fix

--- app/root.tsx
+++ app/root.tsx
@@ -1,15 +1,34 @@
-import { Links, Meta, Outlet, Scripts, ScrollRestoration, useRouteError } from "@remix-run/react";
+import {
+  json,
+  Links,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+  useLoaderData,
+  useRouteError,
+} from "@remix-run/react";
 import "dotenv/config";
-import ReactGA from "react-ga4";
+import { useEffect } from "react";
+import { pageview } from "./lib/gtags.client";
 import "./tailwind.css";
 
-if (typeof window !== "undefined") {
-  ReactGA.initialize("G-JG2X5QBTD3");
+export async function loader() {
+  return json({ gaTrackingId: process.env.REACT_APP_GOOGLE_ANALYTICS_ID });
 }
 
 export function Layout({ children }: { children: React.ReactNode }) {
   const error = useRouteError();
+  const { gaTrackingId } = useLoaderData<typeof loader>();
+
   console.error(error);
+
+  useEffect(() => {
+    if (typeof window !== "undefined" && gaTrackingId?.length) {
+      pageview(location.pathname, gaTrackingId);
+    }
+  }, [gaTrackingId]);
+
   return (
     <html lang="en">
       <head>
@@ -17,6 +36,26 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
+        {process.env.NODE_ENV === "development" || !gaTrackingId ? null : (
+          <>
+            <script async src={`https://www.googletagmanager.com/gtag/js?id=${gaTrackingId}`} />
+            <script
+              async
+              id="gtag-init"
+              dangerouslySetInnerHTML={{
+                __html: `
+                      window.dataLayer = window.dataLayer || [];
+                      function gtag(){dataLayer.push(arguments);}
+                      gtag('js', new Date());
+
+                      gtag('config', '${gaTrackingId}', {
+                        page_path: window.location.pathname,
+                      });
+                    `,
+              }}
+            />
+          </>
+        )}
       </head>
       <body>
         <main className="container relative">
