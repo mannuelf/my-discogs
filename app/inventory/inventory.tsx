e44880c52b2670e4717bfe50fe794161763a2eb6
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2024-07-10 20:35:34 +0200

    styles: update layout card design

--- app/inventory/inventory.tsx
+++ app/inventory/inventory.tsx
@@ -11,25 +11,32 @@ import type { InventoryFetchResponse, Listing } from "./inventory.types";
 
 export const Inventory = (data: InventoryFetchResponse): React.ReactElement => {
   return (
-    <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 xxl:grid-cols-6 gap-4 justify-items-start">
+    <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4 justify-items-start">
       {data.listings.map((listing: Listing) => (
-        <article key={listing.release.title} className="flex justify-start w-full">
+        <article
+          key={listing.release.title}
+          className="flex justify-start w-full"
+        >
           <Card className={cn("p-0 shadow-none w-full overflow-hidden")}>
             <div className="justify-items-start">
               <CardHeader
-                className="flex-1 h-80 p-0"
+                className="flex-1 h-80 md:h-80 lg:h-90 p-0"
                 style={{
                   backgroundImage: `url(${listing.release.images[0].uri})`,
                   backgroundSize: "cover",
                   backgroundPosition: "center",
                 }}
-              ></CardHeader>
+              >
+              </CardHeader>
               <CardContent className="flex-1 pt-4">
-                <CardTitle className="text-xl">{listing.release.title}</CardTitle>
+                <CardTitle className="text-xl">
+                  {listing.release.title}
+                </CardTitle>
                 <CardDescription className="leading-6 text-black">
                   <strong>Artist:</strong> {listing.release.artist}
                   <br />
-                  <strong>Label:</strong> {listing.release.label} - {listing.release.catalog_number}
+                  <strong>Label:</strong> {listing.release.label} -{" "}
+                  {listing.release.catalog_number}
                   <br />
                   <strong>Released:</strong> {listing.release.year}
                   <br />
@@ -52,7 +59,12 @@ export const Inventory = (data: InventoryFetchResponse): React.ReactElement => {
                       className="flex justify-start text-sm text-white border-0 border-black-600 bg-black hover:text-color-black rounded-md p-2 mt-2"
                     >
                       <span className="w-[24px] mt-[2px]">
-                        <img src={"./icon-youtube.svg"} alt="YouTube" width={16} height={16} />
+                        <img
+                          src={"./icon-youtube.svg"}
+                          alt="YouTube"
+                          width={16}
+                          height={16}
+                        />
                       </span>
                       <span>Listen</span>
                     </a>
