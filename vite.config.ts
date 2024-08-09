3cd3ffe227d55355f16b5d4eda12b61154206b48
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2024-08-10 00:19:55 +0200

    fix: console log errors, clean up

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,9 @@
 import { vitePlugin as remix } from "@remix-run/dev";
-import { defineRdtConfig, remixDevTools } from "remix-development-tools";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-const customConfig = defineRdtConfig({
-  client: {
-    position: "top-right",
-    defaultOpen: false,
-    expansionLevel: 1,
-    height: 500,
-    minHeight: 300,
-    maxHeight: 1000,
-    hideUntilHover: true,
-    panelLocation: "bottom",
-    requireUrlFlag: true,
-    urlFlag: "customFlag",
-    routeBoundaryGradient: "gotham",
-  },
-});
-
 export default defineConfig({
   plugins: [
-    remixDevTools(customConfig),
     remix({
       future: {
         v3_fetcherPersist: true,
