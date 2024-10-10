50312b4e6f850657208834059babb649370067b4
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2024-10-10 21:48:04 +0200

    feat: refactors out to helpers file

--- app/inventory/index.ts
+++ app/inventory/index.ts
@@ -1,40 +1,10 @@
-import { getErrorMessage } from "./inventory.helpers";
-import type { InventoryFetchResponse } from "./inventory.types";
-
-const buildUrl = (username: string, params: Record<string, string>): string => {
-  const baseUrl = `https://api.discogs.com/users/${username}/inventory`;
-  const queryParams = new URLSearchParams(params).toString();
-  return `${baseUrl}?${queryParams}`;
-};
-
-const buildHeaders = (): Record<string, string> => ({
-  Authorization: `Discogs key=${process.env.CONSUMER_KEY}, secret=${process.env.CONSUMER_SECRET}`,
-  "User-Agent": `${process.env.USER_AGENT}`,
-});
-
-const validateResponse = async (response: Response): Promise<void> => {
-  if (!response.ok) {
-    const errorMessage = getErrorMessage(response.status);
-    console.error(
-      `Server responded with a status: ${response.status} ${response.statusText}. ${errorMessage}`,
-    );
-    throw new Error(`HTTP error! status: ${response.status}`);
-  }
-};
-
-const parseResponseData = async (response: Response): Promise<InventoryFetchResponse> => {
-  const data = await response.json();
-  if (!data || !data.pagination) {
-    console.error("Unexpected API response structure:", data);
-    throw new Error("Invalid API response structure");
-  }
-  return data;
-};
-
-const handleError = (error: unknown): never => {
-  console.error(`Failed to fetch user inventory:`, error);
-  throw error;
-};
+import {
+  buildHeaders,
+  buildUrl,
+  handleError,
+  parseResponseData,
+  validateResponse,
+} from "./inventory.helpers";
 
 /**
  * Fetch user inventory
--- app/inventory/inventory.helpers.ts
+++ app/inventory/inventory.helpers.ts
@@ -1,4 +1,5 @@
 import { responses } from "types";
+import { InventoryFetchResponse } from "./inventory.types";
 
 export const getErrorMessage = (statusCode: number): string => {
   const response = responses.find((response) => response.status === statusCode);
@@ -7,4 +8,39 @@ export const getErrorMessage = (statusCode: number): string => {
   } else {
     return "An unknown error occurred.";
   }
+};
+
+export const buildUrl = (username: string, params: Record<string, string>): string => {
+  const baseUrl = `https://api.discogs.com/users/${username}/inventory`;
+  const queryParams = new URLSearchParams(params).toString();
+  return `${baseUrl}?${queryParams}`;
+};
+
+export const buildHeaders = (): Record<string, string> => ({
+  Authorization: `Discogs key=${process.env.CONSUMER_KEY}, secret=${process.env.CONSUMER_SECRET}`,
+  "User-Agent": `${process.env.USER_AGENT}`,
+});
+
+export const validateResponse = async (response: Response): Promise<void> => {
+  if (!response.ok) {
+    const errorMessage = getErrorMessage(response.status);
+    console.error(
+      `Server responded with a status: ${response.status} ${response.statusText}. ${errorMessage}`,
+    );
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+};
+
+export const parseResponseData = async (response: Response): Promise<InventoryFetchResponse> => {
+  const data = await response.json();
+  if (!data || !data.pagination) {
+    console.error("Unexpected API response structure:", data);
+    throw new Error("Invalid API response structure");
+  }
+  return data;
+};
+
+export const handleError = (error: unknown): never => {
+  console.error(`Failed to fetch user inventory:`, error);
+  throw error;
 };
